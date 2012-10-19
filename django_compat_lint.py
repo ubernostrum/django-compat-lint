c6c21ac2874e712a4c4f9e8e9007a57fab057fea
James Bennett <james@b-list.org>
2012-10-19 03:09:30 -0500

    Initial commit, with the basic engine and a test ruleset.

---
+++ django_compat_lint.py
@@ -0,0 +1,77 @@
+from optparse import OptionParser
+import os
+
+from django.utils.importlib import import_module
+
+
+RULES = []
+
+usage = 'usage: %prog OPTIONS file1 file2 ...'
+
+MESSAGE_TEMPLATE = '%s:%s:%s:%s'
+
+
+def register_rules(rules_file):
+    global RULES
+    module = import_module('rules.%s' % rules_file)
+    RULES += module.rules
+
+
+def setup():
+    self_dir = os.path.abspath(os.path.dirname(__file__))
+    rules_dir = os.path.join(self_dir, 'rules')
+    for f in os.listdir(rules_dir):
+        if f != '__init__.py' and '.pyc' not in f:
+            register_rules(f.replace('.py', ''))
+
+
+def format_messages(messages, line, filename, level):
+    return [MESSAGE_TEMPLATE % (level, filename, line, message) for \
+            message in messages]
+
+
+def check_file(filename, enabled_rules, options):
+    messages = []
+    lines = open(os.path.abspath(filename)).readlines()
+    for i, line in enumerate(lines):
+        for rule in enabled_rules:
+            warnings, errors, info = rule(line, filename, options)
+            if options.level in ('warnings', 'all'):
+                messages += format_messages(warnings, i + 1, filename, 'WARNING')
+            if options.level in ('errors', 'all'):
+                messages += format_messages(errors, i + 1, filename, 'ERROR')
+            if options.level in ('info', 'all'):
+                messages += format_messages(info, i + 1, filename, 'INFO')
+    return messages
+
+
+if __name__ == '__main__':
+    setup()
+
+    parser = OptionParser(usage)
+    parser.add_option('-l', '--level', dest='level',
+                      action='store',
+                      type='string',
+                      default='errors',
+                      help="Level of messages to display. 'errors', 'warnings', 'info' or 'all'. Default 'errors'.")
+
+    for rule in RULES:
+        parser.add_option(rule['option'], rule['long_option'],
+                          dest=rule['dest'], action=rule['action'],
+                          help=rule['help'])
+
+    options, args = parser.parse_args()
+    if not args:
+        parser.error('At least one filename must be supplied.')
+
+    enabled_rules = []
+    for rule in RULES:
+        if rule['enabled'](options):
+            enabled_rules.append(rule['callback'])
+    
+    messages = []
+    for filename in args:
+        messages += check_file(filename, enabled_rules, options)
+        
+    for message in messages:
+        print message
---
+++ rules/django_utils.py
@@ -0,0 +1,105 @@
+import re
+
+
+DJANGO_UTILS_PATTERN = re.compile(r'(from django\.utils import |import django\.utils\.|from django\.utils\.)(?P<module>\w+)')
+
+
+SAFE_UTILS_MODULES = [
+    'cache',
+    'encoding',
+    'feedgenerator',
+    'http',
+    'safestring',
+    'translation',
+    'tzinfo',
+]
+
+DJANGO_14_UTILS = [
+    '_os',
+    'archive',
+    'autoreload',
+    'baseconv',
+    'cache',
+    'checksums',
+    'copycompat',
+    'crypto',
+    'daemonize',
+    'datastructures',
+    'dateformat',
+    'dateparse',
+    'dates',
+    'datetime_safe',
+    'decorators',
+    'dictconfig',
+    'encoding',
+    'feedgenerator',
+    'formats',
+    'functional',
+    'hashcompat',
+    'html',
+    'html_parser',
+    'http',
+    'importlib',
+    'ipv6',
+    'itercompat',
+    'jslex',
+    'log',
+    'module_loading',
+    'numberformat',
+    'regex_helper',
+    'safestring',
+    'simplejson',
+    'six',
+    'synch',
+    'termcolors',
+    'text',
+    'timesince',
+    'timezone',
+    'translation',
+    'tree',
+    'tzinfo',
+    'unittest',
+    'version',
+    'xmlutils',
+]
+
+UTILS_STDLIB_MAP = {
+    'functional': 'functools',
+    'simplejson': 'json',
+    '_threading_local': 'threading',
+    'thread_support': 'threading',
+}
+
+
+def check_utils(line, filename, options):
+    warnings = []
+    errors = []
+    info = []
+    module = None
+    django_utils = DJANGO_UTILS_PATTERN.search(line)
+    if django_utils and 'import' in line:
+        module = django_utils.groupdict()['module']
+        if module in SAFE_UTILS_MODULES:
+            return warnings, errors, info
+        if module not in DJANGO_14_UTILS:
+            errors.append(
+                'django.utils.%s does not exist in Django 1.4.' % module)
+        if module in UTILS_STDLIB_MAP:
+            info.append(
+                'Use %s in stdlib instead of %s.' % (
+                    UTILS_STDLIB_MAP[module], module))
+        if module != 'datastructures' and 'SortedDict' not in line:
+            warnings.append(
+                'django.utils.%s is not considered stable public API.' % module)
+    return warnings, errors, info
+
+
+rules = [
+    {'option': '-u',
+     'long_option': '--utils',
+     'action': 'store_true',
+     'dest': 'utils',
+     'help': 'Check for unsafe use of django.utils.',
+     'callback': check_utils,
+     'enabled': lambda options: options.utils,}
+]
